8e8272ba6822f604890643d59ac79a24348f0581
Gordon Sun <gsun@snackv.com>
2019-01-18 16:24:48 -0800

    Add load async

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Video } from 'expo';
 
 export default class App extends React.Component {
+  playerRef = undefined
+  async componentDidMount() {
+    try {
+      console.log('calling loadAsync')
+      await this.playerRef.loadAsync(
+        {uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4'},
+        {shouldPlay: true}
+      )
+      console.log('load async success')
+    }
+    catch(e){
+      console.log('load async failed')
+      console.log(e)
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Video
+          // source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+          rate={1.0}
+          volume={1.0}
+          isMuted={false}
+          resizeMode="cover"
+          shouldPlay
+          isLooping
+          style={{ width: 300, height: 300 }}
+          ref={(ref) => {this.playerRef = ref}}
+        />
       </View>
     );
   }
